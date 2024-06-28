bd84a76dc757dcea8863002813f835cd48ba6139
tomer00gud <himanshutomer00@gmail.com>
2024-06-28 21:25:31 +0530

    Msg Types Updated

--- app/src/main/java/com/tomer/chitchat/modals/msgs/Acknowledge.kt
+++
@@ -1,9 +0,0 @@
-package com.tomer.chitchat.modals.msgs
-
-import javax.inject.Inject
-
-data class Acknowledge(
-    val t:Int
-){
-    override fun toString(): String = ""
-}
---
+++ app/src/main/java/com/tomer/chitchat/modals/msgs/Message.kt
@@ -0,0 +1,22 @@
+package com.tomer.chitchat.modals.msgs
+
+abstract class Message(
+    private val msg:String
+) {
+    override fun toString() = msg
+}
+
+data class Acknowledge(
+    val msgId:String
+):Message(
+    "*-ACK-*$msgId"
+)
+
+// <(10)toPhone>  <(7)MSG_TYPE>
+// <DATA>  { <(12) TEMPID><ENC DATA> }
+data class ChatMessage(
+    val msgId:String,
+    val msg:String
+):Message(
+    "*-MSG-*$msgId$msg"
+)
