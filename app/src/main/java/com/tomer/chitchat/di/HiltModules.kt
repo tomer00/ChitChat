87a6ce6a9e02be237621dcf70a873462a89bb484
tomer00gud <himanshutomer00@gmail.com>
2024-07-05 11:03:43 +0530

    Room Custom Call Adapter

--- app/src/main/java/com/tomer/chitchat/di/HiltModules.kt
+++ app/src/main/java/com/tomer/chitchat/di/HiltModules.kt
@@ -1,22 +1,28 @@
 package com.tomer.chitchat.di
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Room
-import com.tomer.chitchat.room.Dao
-import com.tomer.chitchat.room.Database
-import com.tomer.chitchat.retro.Api
-import com.tomer.chitchat.utils.Utils
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.tomer.chitchat.assets.RepoAssets
+import com.tomer.chitchat.assets.WebAssetsRepo
 import com.tomer.chitchat.repo.RepoMessages
 import com.tomer.chitchat.repo.RepoMessagesImpl
 import com.tomer.chitchat.repo.RepoUtilImpl
 import com.tomer.chitchat.repo.RepoUtils
+import com.tomer.chitchat.retro.Api
+import com.tomer.chitchat.retro.SyncCallAdapterFactory
+import com.tomer.chitchat.room.Dao
+import com.tomer.chitchat.room.Database
+import com.tomer.chitchat.utils.CipherUtils
+import com.tomer.chitchat.utils.Utils
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -27,9 +33,19 @@ class HiltModules {
 
     @Provides
     @Singleton
-    fun provideRetroApiCompiler(): Api {
+    fun provideRetroApiCompiler(repoUtils: RepoUtils): Api {
         return Retrofit.Builder().baseUrl(Utils.SERVER_LINK)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().serializeNulls().create()))
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(SyncCallAdapterFactory())
+            .client(OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val originalRequest = chain.request()
+                    val authenticatedRequest = originalRequest.newBuilder()
+                        .header("Authorization", "Bearer ${repoUtils.getToken()}")
+                        .build()
+                    chain.proceed(authenticatedRequest)
+                }
+                .build())
             .build()
             .create(Api::class.java)
     }
@@ -38,6 +54,11 @@ class HiltModules {
     @Singleton
     fun provideGson(): Gson = Gson()
 
+
+    @Provides
+    @Singleton
+    fun provideCipher(): CipherUtils = CipherUtils()
+
     @Provides
     @Singleton
     fun provideRepoMsgs(dao: Dao): RepoMessages = RepoMessagesImpl(dao)
@@ -45,6 +66,10 @@ class HiltModules {
     @Provides
     @Singleton
     fun provideRepoUtils(@ApplicationContext appContext: Context): RepoUtils = RepoUtilImpl(appContext)
+
+    @Provides
+    @Singleton
+    fun provideRepoAssets(@ApplicationContext appContext: Context): RepoAssets = WebAssetsRepo(appContext)
 
 
     @Provides
--- app/src/main/java/com/tomer/chitchat/retro/Api.kt
+++ app/src/main/java/com/tomer/chitchat/retro/Api.kt
@@ -1,6 +1,7 @@
 package com.tomer.chitchat.retro
 
-import retrofit2.Call
+import com.tomer.chitchat.retro.modals.LoginResponse
+import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -9,7 +10,14 @@ interface Api {
 
     @FormUrlEncoded
     @POST("/login")
-    fun getLoginToken(@Field("token") token: String): Call<TokenResp>
+    suspend fun getLoginToken(
+        @Field("token") token: String
+    ): Response<LoginResponse>
 
+    @FormUrlEncoded
+    @POST("/update/name")
+    suspend fun updateName(
+        @Field("name") name: String
+    ): Response<String>
 
 }
