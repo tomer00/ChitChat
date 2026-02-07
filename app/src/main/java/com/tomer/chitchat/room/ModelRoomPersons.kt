a53b994b8788ab08f3c81e697ea25cd88a46c93a
tomer00 <himanshutomer00@gmail.com>
2026-02-07 21:23:18 +0530

    Added @ColumnInfo annotations to ModelRoomPersons fields

--- app/src/main/java/com/tomer/chitchat/room/ModelRoomPersons.kt
+++ app/src/main/java/com/tomer/chitchat/room/ModelRoomPersons.kt
@@ -1,5 +1,6 @@
 package com.tomer.chitchat.room
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.tomer.chitchat.modals.states.MsgStatus
@@ -7,14 +8,33 @@ import com.tomer.chitchat.modals.states.MsgStatus
 @Entity(tableName = "persons")
 data class ModelRoomPersons(
     @PrimaryKey
+    @ColumnInfo(name = "phoneNo")
     val phoneNo: String,
+
+    @ColumnInfo(name = "name")
     val name: String,
+
+    @ColumnInfo(name = "mediaType")
     val mediaType: MsgMediaType,
+
+    @ColumnInfo(name = "lastMsg")
     val lastMsg: String,
+
+    @ColumnInfo(name = "lastMsgId")
     val lastMsgId: Long,
+
+    @ColumnInfo(name = "timeMillis")
     var timeMillis: Long,
+
+    @ColumnInfo(name = "unReadCount")
     var unReadCount: Int = 0,
+
+    @ColumnInfo(name = "lastSeenMillis")
     var lastSeenMillis: Long,
+
+    @ColumnInfo(name = "isSent")
     val isSent: Boolean,
+
+    @ColumnInfo(name = "msgStatus")
     var msgStatus: MsgStatus
 )
