ce32137363ee63decfa7a60cca0c4a405dce8eb0
tomer00 <himanshutomer00@gmail.com>
2026-02-08 21:24:53 +0530

    Added ColumnInfo annotations to ModelRoomPersonRelation fields

--- app/src/main/java/com/tomer/chitchat/room/ModelRoomPersonRelation.kt
+++ app/src/main/java/com/tomer/chitchat/room/ModelRoomPersonRelation.kt
@@ -1,13 +1,21 @@
 package com.tomer.chitchat.room
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "relation")
 data class ModelRoomPersonRelation(
     @PrimaryKey
-    val partnerId : String,
-    var isConnSent : Boolean,
-    var isAccepted : Boolean,
-    var isRejected : Boolean
+    @ColumnInfo(name = "partnerId")
+    val partnerId: String,
+
+    @ColumnInfo(name = "isConnSent")
+    var isConnSent: Boolean,
+
+    @ColumnInfo(name = "isAccepted")
+    var isAccepted: Boolean,
+
+    @ColumnInfo(name = "isRejected")
+    var isRejected: Boolean
 )
