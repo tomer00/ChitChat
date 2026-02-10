b5e9c7e3bad1f6ccf7a348545caf30eeb29bb20e
tomer00 <himanshutomer00@gmail.com>
2026-02-10 12:47:42 +0530

    Reformatted Message.kt for improved code style and readability

--- app/src/main/java/com/tomer/chitchat/modals/msgs/Message.kt
+++ app/src/main/java/com/tomer/chitchat/modals/msgs/Message.kt
@@ -2,27 +2,21 @@ package com.tomer.chitchat.modals.msgs
 
 import com.tomer.chitchat.utils.Utils
 
-abstract class Message(
-    private val msg: String
-) {
+abstract class Message(private val msg: String) {
     override fun toString() = msg
 }
 
 class Typing : Message("*-TYP-*")
-
 class NoTyping : Message("*N-TYP*")
 
 class NewConnection(publicSecretHEX: String) : Message("*-NEW-*$publicSecretHEX,-,${Utils.myName}")
-class AcceptConnection(publicSecretHEX: String) : Message("*F-ACC*$publicSecretHEX,-,${Utils.myName}")
+class AcceptConnection(publicSecretHEX: String) :
+    Message("*F-ACC*$publicSecretHEX,-,${Utils.myName}")
+
 class RejectConnection() : Message("*F-REJ*")
 class BulkReceived(toPhone: String, list: String) : Message("$toPhone*MSG-BR$list")
 class OfflineStatus() : Message("*P-STA*")
 
 // <(10)toPhone>  <(7)MSG_TYPE>
 // <DATA>  { <(12) TEMPID><ENC DATA> }
-class ChatMessage(
-    msgId: String,
-    msg: String
-) : Message(
-    "*-MSG-*$msgId$msg"
-)
+class ChatMessage(msgId: String, msg: String) : Message("*-MSG-*$msgId$msg")
