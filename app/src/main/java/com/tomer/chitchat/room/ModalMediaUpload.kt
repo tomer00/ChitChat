98ebb9151b538c71538c6b66d59379fbc1346a77
tomer00 <himanshutomer00@gmail.com>
2026-02-12 12:48:59 +0530

    Added ColumnInfo annotations to ModalMediaUpload entity fields

--- app/src/main/java/com/tomer/chitchat/room/ModalMediaUpload.kt
+++ app/src/main/java/com/tomer/chitchat/room/ModalMediaUpload.kt
@@ -1,11 +1,14 @@
 package com.tomer.chitchat.room
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "media")
 data class ModalMediaUpload(
     @PrimaryKey
+    @ColumnInfo(name = "uri")
     val uri: String,
+    @ColumnInfo(name = "localFileName")
     val localFileName: String
 )
