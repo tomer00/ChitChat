30193b1a98df4f40c1f6dd9ffff6d42a4ccf1d12
tomer00 <himanshutomer00@gmail.com>
2026-02-13 12:49:25 +0530

    Added ColumnInfo annotations to ModelPartnerPref fields

--- app/src/main/java/com/tomer/chitchat/room/ModelPartnerPref.kt
+++ app/src/main/java/com/tomer/chitchat/room/ModelPartnerPref.kt
@@ -1,6 +1,7 @@
 package com.tomer.chitchat.room
 
 import androidx.annotation.IntRange
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.tomer.chitchat.utils.qrProvider.RenderModel
@@ -8,15 +9,34 @@ import com.tomer.chitchat.utils.qrProvider.RenderModel
 @Entity(tableName = "partner_prefs")
 data class ModelPartnerPref(
     @PrimaryKey
+    @ColumnInfo(name = "phone")
     val phone: String,
+
+    @ColumnInfo(name = "name")
     var name: String,
+
+    @ColumnInfo(name = "background")
     var background: RenderModel,
+
+    @ColumnInfo(name = "accent")
     var accent: RenderModel,
+
+    @ColumnInfo(name = "backgroundAssetNo")
     var backgroundAssetNo: Int,
+
+    @ColumnInfo(name = "dpNo")
     var dpNo: Int,
+
+    @ColumnInfo(name = "about")
     var about: String,
-    @IntRange(0, Long.MAX_VALUE)
+
+    @param:IntRange(0, Long.MAX_VALUE)
+    @ColumnInfo(name = "lastOnlineMillis")
     val lastOnlineMillis: Long,
+
+    @ColumnInfo(name = "notificationAllowed")
     var notificationAllowed: Boolean,
+
+    @ColumnInfo(name = "chatLocked")
     var chatLocked: Boolean,
 )
